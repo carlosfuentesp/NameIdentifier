5ca8498b34e510a078c7a5bfccc17d4a85d38003
Carlos Fuentes <cfuentes@thoughtworks.com>
2020-05-11 13:21:10 -0500

    Read people's name from csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,34 @@
+import com.opencsv.CSVReader;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.tokenize.SimpleTokenizer;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.util.Span;
 
-import java.io.FileInputStream;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-            String word = "Carlos";
-
-            NameFinderME finder = new NameFinderME(new TokenNameFinderModel(new FileInputStream("en-ner-person.bin")));
-            Tokenizer tokenizer = SimpleTokenizer.INSTANCE;
-            String[] tokens = tokenizer.tokenize(word);
-            Span[] names = finder.find(tokens);
-
-            displayResult(finder.probs(names), tokens[0], names);
-
-            finder.clearAdaptiveData();
-
+            for (List<String> name : readCsv()) {
+                findName(name.get(0));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void findName(String word) throws IOException {
+        NameFinderME finder = new NameFinderME(new TokenNameFinderModel(new FileInputStream("en-ner-person.bin")));
+        Tokenizer tokenizer = SimpleTokenizer.INSTANCE;
+        String[] tokens = tokenizer.tokenize(word);
+        Span[] names = finder.find(tokens);
+
+        displayResult(finder.probs(names), tokens[0], names);
+
+        finder.clearAdaptiveData();
     }
 
     private static void displayResult(double[] probs, String token, Span[] names) {
@@ -31,6 +36,21 @@ public class Main {
             System.out.println(token + ": Yes, " + probs[0]);
         } else {
             System.out.println(token + ": No");
+        }
+    }
+
+    private static List<List<String>> readCsv() {
+        List<List<String>> records = new ArrayList<List<String>>();
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader("names.csv"));
+            String[] values;
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+            return records;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
         }
     }
 }
